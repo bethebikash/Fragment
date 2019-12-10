174132081e304bf4125b4692701a926d15c01095
bethebikash <bhattaraibikash55@gmail.com>
2019-12-10 08:48:21 +0545

    calculation using fragment.

--- app/src/main/java/com/bhattaraibikash/fragment/fragments/FirstFragment.java
+++ app/src/main/java/com/bhattaraibikash/fragment/fragments/FirstFragment.java
@@ -1,6 +1,7 @@
 package com.bhattaraibikash.fragment.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bhattaraibikash.fragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements View.OnClickListener {
+
+    private Button btnCalculate;
+    private EditText etFirst,etSecond;
 
 
     public FirstFragment() {
@@ -26,7 +33,23 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        etFirst = view.findViewById(R.id.etFirst);
+        etSecond = view.findViewById(R.id.etSecond);
+        btnCalculate = view.findViewById(R.id.btnCalculate);
+
+        btnCalculate.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int first = Integer.parseInt(etFirst.getText().toString());
+        int second = Integer.parseInt(etSecond.getText().toString());
+        int result = first + second;
+
+        Toast.makeText(getActivity(), "Sum is : " + result, Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/com/bhattaraibikash/fragment/fragments/SecondFragment.java
+++ app/src/main/java/com/bhattaraibikash/fragment/fragments/SecondFragment.java
@@ -8,13 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bhattaraibikash.fragment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SecondFragment extends Fragment {
+public class SecondFragment extends Fragment implements View.OnClickListener {
+
+    private Button btnCalculateArea;
+    private EditText etRadius;
 
 
     public SecondFragment() {
@@ -26,7 +32,21 @@ public class SecondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        etRadius = view.findViewById(R.id.etRadius);
+        btnCalculateArea = view.findViewById(R.id.btnCalculateArea);
+
+        btnCalculateArea.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        float radius = Float.parseFloat(etRadius.getText().toString());
+        float area = 3.143f * radius * radius;
+
+        Toast.makeText(getActivity(),"Area of Circle is : " + area, Toast.LENGTH_SHORT).show();
+    }
 }
